e28aa8f4eb3b6c7ddf20bb0caa7b8c48bb98ecfd
ayuwidyaagata <ayuwagata@gmail.com>
2026-04-08 13:48:05 +0700

    feat: implement my banks page

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,8 +1,38 @@
-export const dynamic = "force-dynamic";
+import BankCard from "@/components/BankCard";
+import HeaderBox from "@/components/HeaderBox"
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-function MyBanks() {
+const MyBanks = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
+
   return (
-    <div>My Banks</div>
+    <section className="flex">
+      <div className="my-banks">
+        <HeaderBox 
+          title="My Bank Accounts"
+          subtext="Effortlessly manage your banking activites."
+        />
+
+        <div className="space-y-4">
+          <h2 className="header-2">
+            Your cards
+          </h2>
+          <div className="flex flex-wrap gap-6">
+            {accounts && accounts.data.map((a: Account, index: number) => (
+              <BankCard 
+                key={index}
+                account={a}
+                userName={loggedIn?.firstName}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
